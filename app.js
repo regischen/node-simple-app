8604a9fbce1ed7509b37399d78534afe9d0a56da
regis <ychen9@umass.edu>
2020-10-24 12:25:30 +0800

    first

---
+++ app.js
@@ -0,0 +1,12 @@
+const express = require('express');
+
+const app = express();
+
+app.get('/', (req, res) => {
+  res.send('hello world')
+})
+
+const port = 5000;
+app.listen(port, () => {
+  console.log('server running at port' + port)
+})
